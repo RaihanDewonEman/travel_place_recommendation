2bff8279d834290d21bfc19b3a7e12c544d67eec
raihan.dewon <raihan.dewon@bracits.com>
2025-07-11 00:13:55 +0600

    Initial project setup

---
+++ config/database.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'travel_recommendation',
+        'USER': 'postgres',
+        'PASSWORD': 'set_your_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
